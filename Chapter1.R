75861501eb0e63d73e0447e851f3f5bcf0bff577
kevinkr <kevinkr@yahoo.com>
2016-08-02 11:32:18 -0500

    thru 1.11

--- Chapter1.R
+++ Chapter1.R
@@ -217,3 +217,123 @@ lmout
 coef(lmout) %∗% c(1,0.525,0.525^2,0,1)
 
 #1.11 Interaction Terms
+#Let's take another look at (1.23), specifically the term involving the variable
+#workingday, a dummy indicating a nonholiday Monday through Friday.
+#Our estimate for B3 turned out to be 988.5, meaning that, holding temperature
+#and the other variables fixed, there are 988.5 additional riders on
+#workingdays.
+#But implicit in this model is that the workingday effect is the same on
+#low-temprerature days as on warmer days. For a broader model that does
+#not make this assumption, we could add an interaction term, consisting of
+#a product of workingday and temp:
+#mean reg = B0 + B1 temp + B2 temp^2 + B3 workingday + B4 clearday + B5 temp * workingday
+#How does this model work? Let's illustrate it with a new data set.
+
+#1.11 Example: Salaries of Female Programmers and Engineers
+#This data is from the 2000 U.S. Census, consisting of 20,090 programmers
+#and engineers in the Silicon Valley area. The data set is included in the
+#freqparcoord package on CRAN. Suppose we are working toward a Description
+#goal, specidically the eddects of gender on wage income.
+
+#As with our bike-sharing data, we'll add a quadratic term, in this case
+#on the age variable, reflecting the fact that many older programmers and
+#engineers encounter trouble finding work after age 35 or so. Let's restrict
+#our analysis to workers having at least a Bachelor's degree, and look at
+#the variables age, age2, sex (coded 1 for male, 2 for female), wkswrked
+#(number of weeks worked), ms, phd and wageinc:
+library (freqparcoord)
+data(prgeng)
+prgeng$age2 <- prgeng$age^2
+edu <- prgeng$educ
+prgeng$ms <- as.integer(edu == 14)
+prgeng$phd <- as.integer(edu == 16)
+prgeng$fem <- prgeng$sex - 1
+tmp <- prgeng[edu >= 13,]
+#select specific columns age, age2, wkswrkd, ms, phd, fem, wageinc
+pe <- tmp[,c(1,12,9,13,14,15,8)]
+pe <- as.matrix(pe)
+
+#out model is:
+#mean wageinc = B0 +B1 age + B2 age^2 + B3 wkswrkd + B4 ms + B5 phd + B6 fem
+#we find the following 
+summary(lm(pe[,7] ~ pe [,−7]))
+
+#Call:
+#lm(formula = pe[, 7] ~ pe[, -7])
+
+#Residuals:
+#  Min     1Q Median     3Q    Max 
+#-99386 -20889  -4567  12467 286564 
+
+#Coefficients:
+#               Estimate Std.      Error   t value  Pr(>|t|)    
+#(Intercept)     -87162.556      4716.088 -18.482   <2e-16 ***
+#  pe[, -7]age       4189.304    234.335  17.877   <2e-16 ***
+#  pe[, -7]age2       -43.479      2.668 -16.293   <2e-16 ***
+#  pe[, -7]wkswrkd   1312.234     29.325  44.748   <2e-16 ***
+#  pe[, -7]ms        9845.719    843.128  11.678   <2e-16 ***
+#  pe[, -7]phd      17403.523   1762.154   9.876   <2e-16 ***
+#  pe[, -7]fem     -11176.740    912.206 -12.252   <2e-16 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Residual standard error: 45440 on 14059 degrees of freedom
+#Multiple R-squared:  0.2035,	Adjusted R-squared:  0.2031 
+#F-statistic: 598.6 on 6 and 14059 DF,  p-value: < 2.2e-16
+
+#The results are striking in terms of gender: With age, education and so on
+#held constant, women are estimated to have incomes about $11,177 lower
+#than comparable men.
+
+#But this analysis implicitly assumes that the female wage deficit is, 
+#for instance, uniform across educational levels. 
+#Being female makes a β6 difference, no matter what the values of ms and
+#phd are. To generalize our model in this regard, let’s define two 
+#interaction variables
+
+msfem <- pe[,4] * pe[,6]
+phdfem <- pe[,5] * pe[,6]
+pe <- cbind(pe, msfem, phdfem)
+
+#Our model is now:
+#mean wageinc = B0 +B1 age + B2 age^2 + B3 wkswrkd + B4 ms + B5 phd + B6 fem
+#               + B7 msfem + B8 phdfem
+
+#So now instead of there being one number for female effect, there are two
+#Female effect for Master's degree holders  B6 + B7
+#Female effect for Phd degree holders  B6 + B8
+#So, let's rerun the regression analysis:
+summary(lm(pe[,7] ~ pe [,-7]))
+
+#Call:
+#lm(formula = pe[, 7] ~ pe[, -7])
+
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-101176  -20938   -4494   12461  284988 
+
+#Coefficients:
+#                   Estimate    Std Error t value   Pr(>|t|)    
+#(Intercept)        -87499.793   4715.343 -18.556  < 2e-16 ***
+#  pe[, -7]age       4183.402    234.244  17.859  < 2e-16 ***
+#  pe[, -7]age2       -43.439      2.667 -16.285  < 2e-16 ***
+#  pe[, -7]wkswrkd   1312.160     29.313  44.763  < 2e-16 ***
+#  pe[, -7]ms       11060.653    965.016  11.462  < 2e-16 ***
+#  pe[, -7]phd      19726.664   1907.382  10.342  < 2e-16 ***
+#  pe[, -7]fem      -9091.230   1121.816  -8.104 5.75e-16 ***
+#  pe[, -7]msfem    -5088.779   1975.841  -2.575  0.01002 *  
+#  pe[, -7]phdfem  -14831.582   4957.859  -2.992  0.00278 ** 
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#Residual standard error: 45420 on 14057 degrees of freedom
+#Multiple R-squared:  0.2043,	Adjusted R-squared:  0.2038 
+#F-statistic: 451.1 on 8 and 14057 DF,  p-value: < 2.2e-16
+
+#The estimated values of the two female effects are -9091.230 -5088.779 =
+#-14180.01, and 9091.230 -14831.582 = -23922.81. A few points jump out
+#here:
+#   Once one factors in educational level, the gender gap is seen to be
+#even worse than before.
+#   The gap is worse at the PhD level than the Master's, likely because
+#of the generally higher wages for the latter.
